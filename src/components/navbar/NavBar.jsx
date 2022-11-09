448e2aace99a0968a202f2b443ad25e54e7f3599
khaled swidan <khaledqutb4@gmail.com>
2022-11-09 21:59:46 +0200

    ....

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -10,21 +10,25 @@ import ShoppingNav from './ShoppingNav';
 
 const NavBar = () =>
 {
-  const { topnav } = styles;
+  const { topnav, settingNav, contact, contSetting } = styles;
   
   return (
     <>
-      <nav className={`navbar ${topnav} pt-0 `}>
+      <nav className={`navbar ${topnav} pt-0`}>
         <div className="container d-flex justify-content-center w-100 py-2 px-5 position-relative border-bottom">
           <Link className="navbar-brand d-flex flex-row-reverse justify-content-center align-items-center" to="/">
             <img src={siteLogo} className="w-25 ps-1" alt="" />
             أسم الصيدلية|
           </Link>
-          <div className="d-flex align-items-center">
-            <address className="mb-0 text-center">عنوان الادارة / الاسكندرية - مصر</address>
-            <a href="tel:123456789" className='text-white ps-1'>03/578499111</a>
-            <ShoppingNav />
-            <ProfileNav />
+          <div className={`${contSetting} d-flex`}>
+            <div className={`${contact} d-flex align-items-center`}>
+              <address className="mb-0 text-center">عنوان الادارة / الاسكندرية - مصر</address>
+              <a href="tel:123456789" className='text-white ps-1'>03/578499111</a>
+            </div>
+            <div className={`${settingNav}`}>
+              <ShoppingNav />
+              <ProfileNav />
+            </div>
           </div>
         </div>
         <div className="container">
